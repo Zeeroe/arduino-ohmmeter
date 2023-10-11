2e16a5e95c3ae1fbe0f780dcb4ec3336626b3e78
Zeeroe <13524382+Zeeroe@users.noreply.github.com>
2023-10-11 03:34:55 -0400

    Update ohmmeter code to include error checking
    - Changed `lightPin` to `VpPin` on line 6
    - Added error checking to ensure `Vout` is not zero before calculating `Runkown` on line 18
    - Removed unnecessary decimal places in `lcd.print` statements on lines 23 and 26
    - Updated comments for clarity

--- ohmmeter.ino
+++ ohmmeter.ino
@@ -1,6 +1,8 @@
-#include <LiquidCrystal.h>        // include the lcd library code
+#include <LiquidCrystal.h> // include the lcd library code
 
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);   // initialize the library with the numbers of the interface pins
+// initialize the library with the numbers of the interface pins
+LiquidCrystal
+    lcd(7, 8, 9, 10, 11, 12);
 int VpPin = A0;
 
 float Vin = 5;
@@ -9,22 +11,29 @@ float Rknown = 3900;
 float Runkown;
 
 void setup() {
-  Serial.begin(9600);
-  
-  lcd.begin(16, 2);               // set up the LCD's number of columns and rows:
+    Serial.begin(9600);
+    lcd.begin(16, 2); // set up the LCD's number of columns and rows:
 }
 
 void loop() {
-  lcd.clear();
-  Vout = analogRead(lightPin) * (Vin / 1023);
-  lcd.setCursor(0, 1);
-  lcd.print("Vout = ");
-  lcd.print(Vout);
-  
-  Runkown = (Vin * Rknown) / Vout - Rknown;
-  lcd.setCursor(0,0);
-  lcd.print("R = ");
-  lcd.print(Runkown);
+    lcd.clear();
+    Vout = analogRead(VpPin) * (Vin / 1023);
+    lcd.setCursor(0, 1);
+    lcd.print("Vout = ");
+    lcd.print(Vout);
 
-  delay(250);
+    if (Vout != 0) {
+        Runkown = (Vin * Rknown) / Vout - Rknown;
+        lcd.setCursor(0, 0);
+        lcd.print("R = ");
+        lcd.print(Runkown);
+    }
+
+    else {
+        lcd.setCursor(0, 0);
+        lcd.print("R = ");
+        lcd.print("inf");
+    }
+
+    delay(250);
 }
